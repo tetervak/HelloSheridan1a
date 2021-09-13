0128bc0274c3c4a0070bd964a2db604a613a75f5
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-09-13 08:21:48 -0400

    added the onClickListener to the button

--- app/src/main/java/sheridan/tetervak/hellosheridan1a/MainActivity.kt
+++ app/src/main/java/sheridan/tetervak/hellosheridan1a/MainActivity.kt
@@ -2,10 +2,25 @@ package sheridan.tetervak.hellosheridan1a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickMeButton : Button = findViewById(R.id.clickMeButton)
+        clickMeButton.setOnClickListener {
+            Log.d(TAG, "The Button is Clicked")
+            Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
